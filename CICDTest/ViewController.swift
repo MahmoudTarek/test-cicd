a58a600f8120e4707370d84e63f64a986c5d02bc
Mahmoud Tarek <mahmoudtarek910@gmail.com>
2019-03-06 12:09:34 +0200

    First commit

---
+++ CICDTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICDTest
+//
+//  Created by Mahmoud Tarek on 3/6/19.
+//  Copyright © 2019 Mahmoud Tarek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
